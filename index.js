ed250185a76fae6290114ec520cfd4be48d2ecb7
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2024-03-18 15:28:07 +0000

    ux for phone's user

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000; // Chọn một cổng bạn muốn dùng
+const port = 10000; 
 
-// Sử dụng các file tĩnh trong thư mục 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route mặc định trả về file index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Lắng nghe các kết nối đến cổng đã chọn
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
